58b8d08bc1535fcb2fabeef012097067cd75277f
Lucas Cantoni <lukcnt@outlook.com>
2022-08-18 09:03:48 -0300

    Create an empty list to display the blank spaces representing each letter of the word

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
+display = []
+for letter in range(len(chosen_word)):
+    display.append("_")
+
 user_choice = input("Make a guess: ")
 guess = user_choice.lower()
 
